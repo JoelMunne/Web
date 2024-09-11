6997393bc9de3f25af349a64a207b037a1da0cb4
JoelMunne <joelmunnne@gmail.com>
2024-09-11 14:14:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3107e3678f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Troba el teu professor</title>
+
+
+</head>
+
+<body>
+    <header>
+        <img src="logo_sense fondo.png" class="logo">
+        <div class="menu">
+            <a href="#" class="alta">Donat d'alta com a professor</a>
+            <a href="#" class="accedir">Accedir</a>
+        </div>
+    </header>
+
+    <div class="spacer">
+    <a href="#" class="buscar">Trobar el meu professor</a>
+</div>
+</body>
+
+</html>
